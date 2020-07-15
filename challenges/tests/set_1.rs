75598b913bff546ca4f8b9e874c63e84a1f66098
Thomas O'Brien <thomas.obrien86@gmail.com>
2020-07-15 22:26:52 +0100

    Add challenge 3

--- challenges/tests/set_1.rs
+++ challenges/tests/set_1.rs
@@ -1,12 +1,13 @@
 mod set_1 {
-    use crypto::common::{base64, hex, xor};
+    use crypto::common::{base64, hex, plaintext_utils, xor};
+    use std::ops::Range;
 
     #[test]
     fn challenge_1() {
         let input = hex::decode("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").unwrap();
         let output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
-        assert_eq!(base64::encode(&input[..]), output)
+        assert_eq!(base64::encode(&input), output)
     }
 
     #[test]
@@ -15,8 +16,34 @@ mod set_1 {
         let y = hex::decode("686974207468652062756c6c277320657965").unwrap();
         let z = hex::decode("746865206b696420646f6e277420706c6179").unwrap();
 
-        let result = xor::xor_buffers(&x[..], &y[..]);
+        let result = xor::xor_buffers(&x, &y);
 
         assert_eq!(result, z)
+    }
+
+    #[test]
+    fn challenge_3() {
+        let input =
+            hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+                .unwrap();
+        let ascii: Range<u8> = 0..0x80;
+
+        let highest_scoring_byte = ascii
+            .map(|b| {
+                let output = xor::xor_buffers(&input, &vec![b; input.len()]);
+
+                (plaintext_utils::score_english_plaintext(&output), b)
+            })
+            .max_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap())
+            .unwrap()
+            .1;
+
+        let best = String::from_utf8(xor::xor_buffers(
+            &input,
+            &vec![highest_scoring_byte; input.len()],
+        ))
+        .unwrap();
+
+        assert_eq!(best, "Cooking MC's like a pound of bacon")
     }
 }
--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -2,4 +2,5 @@ pub mod common {
     pub mod base64;
     pub mod hex;
     pub mod xor;
+    pub mod plaintext_utils;
 }
