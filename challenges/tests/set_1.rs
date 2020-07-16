82d084e63b0967dcd5684726f165ef678817222d
Thomas O'Brien <thomas.obrien86@gmail.com>
2020-07-16 22:49:36 +0100

    Add set 1 challenge 5

--- challenges/tests/set_1.rs
+++ challenges/tests/set_1.rs
@@ -87,4 +87,21 @@ mod set_1 {
             "Now that the party is jumping\n"
         )
     }
+
+    #[test]
+    fn challenge_5() {
+        let input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+            .as_bytes();
+        let key: Vec<u8> = "ICE"
+            .as_bytes()
+            .into_iter()
+            .cycle()
+            .take(input.len())
+            .map(|x| *x)
+            .collect();
+
+        let output = xor::xor_buffers(input, &key);
+
+        assert_eq!(output, hex::decode("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f").unwrap())
+    }
 }
