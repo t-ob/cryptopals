e9f643abb35c2f7979a5fbb131a466b9a8f7f5ea
Tom O'Brien <thomas.obrien86@gmail.com>
2022-10-14 10:48:13 +0100

    Common code

--- src/bin/single-byte-xor.rs
+++ src/bin/single-byte-xor.rs
@@ -1,56 +1,8 @@
-use std::collections::HashMap;
 use std::io;
 
 use cryptopals::hex;
+use cryptopals::plaintext_util;
 use cryptopals::xor;
-
-fn score(buffer: &[u8]) -> u32 {
-    let freqs = HashMap::from([
-        (b' ', 15),
-        (b'e', 13),
-        (b'E', 13),
-        (b't', 9),
-        (b'T', 9),
-        (b'a', 8),
-        (b'A', 8),
-        (b'o', 7),
-        (b'O', 7),
-        (b'i', 7),
-        (b'I', 7),
-        (b'n', 6),
-        (b'N', 6),
-        (b's', 6),
-        (b'S', 6),
-        (b'h', 6),
-        (b'H', 6),
-        (b'r', 6),
-        (b'R', 6),
-        (b'd', 4),
-        (b'D', 4),
-        (b'l', 4),
-        (b'L', 4),
-        (b'c', 2),
-        (b'C', 2),
-        (b'u', 2),
-        (b'U', 2),
-        (b'm', 2),
-        (b'M', 2),
-        (b'w', 2),
-        (b'W', 2),
-        (b'f', 2),
-        (b'F', 2),
-        (b'g', 2),
-        (b'G', 2),
-        (b'y', 2),
-        (b'Y', 2),
-        (b'p', 1),
-        (b'P', 1),
-        (b'b', 1),
-        (b'B', 1),
-    ]);
-
-    buffer.iter().map(|b| freqs.get(b).unwrap_or(&0)).sum()
-}
 
 fn main() -> io::Result<()> {
     let ciphertext_hex = io::stdin().lines().collect::<Result<String, _>>()?;
@@ -63,7 +15,7 @@ fn main() -> io::Result<()> {
 
         let plaintext_candidate = xor::xor_buffers(&ciphertext, &key_candidate);
 
-        scores[byte as usize] = score(&plaintext_candidate);
+        scores[byte as usize] = plaintext_util::english_score(&plaintext_candidate);
         plaintext_candidates[byte as usize].copy_from_slice(&plaintext_candidate);
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod base64;
 pub mod common;
 pub mod hex;
+pub mod plaintext_util;
 pub mod xor;
